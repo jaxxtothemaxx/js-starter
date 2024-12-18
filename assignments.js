c56057d06b7d572cd7adfd3ca8eb4b6511b49cdc
jaxxtothemaxx <49996406+jaxxtothemaxx@users.noreply.github.com>
2024-12-17 23:24:59 -0500

    Add assignments file, update script file, remove text file, link assignments file in index.html

---
+++ assignments.js
@@ -0,0 +1,8 @@
+let country = 'United States of America';
+let continent = 'North America';
+let population = 335.9;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
--- script.js
+++ script.js
@@ -1,3 +1,49 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas');
+// console.log('23');
+
+// let firstName = 'Matilda';
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// //Variable name convention
+// let jonas_matilda = 'JM';
+// let $function = 27;
+
+// let person = 'jonas';
+// let PI = 3.1415;
+
+// let myFirstJob = 'Coder';
+// let myCurrentJob = 'Teacher';
+
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob);
+
+true;
+console.log(javaScriptIsFun);
+let javaScriptIsFun = true;
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
