66ff233edd1c6c80bc61bdbc12dfddc13f151b5e
jaxxtothemaxx <49996406+jaxxtothemaxx@users.noreply.github.com>
2024-12-19 16:55:29 -0500

    Changes to script.js file
    Variables, math operators, assignment operators

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// let js = 'amazing';
-// console.log(40 + 8 + 23 - 10);
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
 
-// console.log('Jonas');
-// console.log('23');
+console.log('Jonas');
+console.log('23');
 
-// let firstName = 'Matilda';
+let firstName = 'Matilda';
 
-// console.log(firstName);
-// console.log(firstName);
-// console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
-// //Variable name convention
-// let jonas_matilda = 'JM';
-// let $function = 27;
+//Variable name convention
+let jonas_matilda = 'JM';
+let $function = 27;
 
-// let person = 'jonas';
-// let PI = 3.1415;
+let person = 'jonas';
+let PI = 3.1415;
 
-// let myFirstJob = 'Coder';
-// let myCurrentJob = 'Teacher';
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
 
-// let job1 = 'programmer';
-// let job2 = 'teacher';
+let job1 = 'programmer';
+let job2 = 'teacher';
 
-// console.log(myFirstJob);
+console.log(myFirstJob);
 
 true;
 console.log(javaScriptIsFun);
@@ -44,6 +44,41 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Shmedtmann';
+console.log(lastName);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3  means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann'
+console.log(firstName + ' ' + lastName);
+
+//Assignment Operators
+let x = 5 + 10;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1
+console.log(x);
 
 
 
