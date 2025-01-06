536b39ec5604bf59cd9ef2b15ab154b071d62dd4
jaxxtothemaxx <49996406+jaxxtothemaxx@users.noreply.github.com>
2025-01-06 17:21:38 -0500

    Type Conversion and Coercion
    adding section to script.js about JS type conversion and coercion

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ console.log(`String with
 multiple
 lines`);
 
-*/
+
 
 //Taking Decisions: if/else statements
 const age = 15;
@@ -152,3 +152,25 @@ if (birthYear < 2000) {
 }
 
 console.log(century);
+*/
+
+//Type Conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//Type Coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+console.log('23' / '2');
+
+// Guess the Output
+let n = '1' + 1; // '11'
+n = n - 1; // 10
+console.log(n);
